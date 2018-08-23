78cf18e629e29b5788abd8634abf08d611f9b385
NAPOLEON039 <nihar.raote@gmail.com>
2018-08-23 19:47:49 +0530

    Added assertion library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,13 +1,24 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     let result = utils.add(15, 5);
+    expect(result).toBe(20).toBeA('number');
 });
 
 it('should square a number', () => {
     let square = utils.square(3);
+    expect(square).toBeA('number').toBe(9);
+});
 
-    if (square !== 9) {
-        throw new Error(`Value expected was 9, but got ${square}`);
-    }
+it('should set full name properly', () => {
+    let user = {
+        age: 19,
+        city: 'Mumbai',
+        country: 'India'
+    };
+    let newUser = utils.setName(user, 'Nihar Raote');
+
+    expect(newUser).toBeA('object').toInclude({firstName: 'Nihar'}).toInclude({lastName: 'Raote'});
+    // expect(newUser).toBeA('object').toInclude({firstName: 'Nihar', lastName: 'Raote'});
 });
